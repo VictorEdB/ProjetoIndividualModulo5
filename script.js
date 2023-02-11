c31192d49cfcfcb8b3d6c6cc0029493117006ca6
VictorEdB <victorbatista2020.vb@gmail.com>
2023-02-11 15:48:46 -0300

    2.0

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ let nome = [];
 let ajuda = "";
 
 
-while(ajuda != 'exit'){
+while(ajuda != 'sair'){
 
     ajuda = readline.question('Digite o nome: ');
     //exemplo: Color
 
     nome.push(ajuda);
 
-    ajuda = readline.question('Para sair digite "exit". Aperte Enter para continuar');
+    ajuda = readline.question('Para sair digite "sair". Aperte Enter para continuar ');
+
 }
 
 console.log(nome.sort().join('\n'));
